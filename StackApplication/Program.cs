8ebb7ac51a0742ab28014f240d3d1f146ab4e1e1
Stanislaw Makievsky <burnfeniks@yandex.ru>
2023-02-10 14:37:54 +0300

    Работа с Stack

---
+++ StackApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LIFO
+            Stack<string> students = new Stack<string>();
+            students.Push("Иван");
+            students.Push("Михаил");
+            students.Push("Сергей");
+            students.Push("Наталья");
+
+            for (int i = 0;  students.Count > 0; i++)
+            {
+                Console.WriteLine(students.Pop()) ;
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }       
+
+            Console.ReadKey();
+        }                
+    }                    
+}
---
+++ StackApplication/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApplication
+{
+    public class TestClass
+    {
+        public static int MyProperty { get; set; }
+        public static int MyProperty2 { get; set; }
+
+    }
+}
